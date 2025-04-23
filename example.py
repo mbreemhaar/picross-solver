1919ef67a0968aa07cd66d7dff11ed60c5b5ce76
Marco Breemhaar <marcobreemhaar@gmail.com>
2025-04-23 14:55:34 +0200

    Add example

---
+++ example.py
@@ -0,0 +1,28 @@
+from picross_solver.puzzle import Puzzle
+
+row_clues = [
+    [4],
+    [1, 1],
+    [1,1,1,1],
+    [1,1,1,1],
+    [1,1],
+    [1,1,1,1],
+    [2,2,2],
+    [1,1],
+    [4]
+]
+col_clues = [
+    [5],
+    [1,2],
+    [1,2,1,1],
+    [1,1,1],
+    [1,1,1],
+    [1,2,1,1],
+    [1,2],
+    [5]
+]
+
+puzzle = Puzzle(row_clues, col_clues)
+puzzle.solve()
+
+print(puzzle)
--- main.py
+++
@@ -1,9 +0,0 @@
-from picross_solver.puzzle import Puzzle
-
-row_clues = [[3,1,1], [1,1,1], [3,2,1], [2,2,1], [6], [1,1], [4], [2], [2,3,1], [1,3,2]]
-col_clues = [[2,1],[1,2,2], [1,1], [1,2,1],[4,1,1],[1,3,1,2],[1,1,1,1],[1,1,1],[1,1,1,1], [2,1,1,3]]
-
-puzzle = Puzzle(row_clues, col_clues)
-puzzle.solve()
-
-print(puzzle)
